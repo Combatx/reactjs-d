064dd71629d0a8f3ed1cc574e4d01e9f670d9880
CombatX <ridrdo@gmail.com>
2022-11-19 20:24:46 +0700

    class component and function component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Welcome from "./FunctionalComponet.js";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn Reactfddf
         </a>
+        <Welcome name="Ridho" />
       </header>
     </div>
   );
